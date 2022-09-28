54b773b7fcfb5713effb2fe9f507aa8d937ab735
Biteto <57534901+Biteto@users.noreply.github.com>
2022-09-28 14:33:15 +0100

    Initial commit

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using countrie.Models;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace countrie.Controllers
+{
+    public class CountriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var result = GetCountries();
+            return View(result);
+        }
+
+        public Countries GetCountries()
+        {
+            var webClient = new WebClient();
+            var json = webClient.DownloadString(@"https://restcountries.com/v3.1/all");
+            var _countries = JsonConvert.DeserializeObject<List<Countrie>>(json);
+            var result = new Countries { countries = _countries };
+            return result;
+        }
+
+        public PartialViewResult searchCountries(string filtro,string regiao)
+        {
+            try
+            {
+                if(filtro!=null)
+                {
+
+                var model = GetCountries();
+                var result = new Countries { countries = model.countries.Where(x => x.name.official.ToLower().Contains(filtro) && x.region.Contains(regiao)).ToList()};
+                return PartialView("_PartialView",result);
+                }
+                else
+                {
+                    var model = GetCountries();
+                    var result = new Countries { countries = model.countries.Where(x => x.region.Contains(regiao)).ToList()};
+
+                    return PartialView("_PartialView", model);
+
+                }
+            }
+            catch
+            {
+                return PartialView();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Countries.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace countrie.Models
+{
+    public class Countries
+    {
+        public List<Countrie> countries { get; set; }
+    }
+    public class Countrie
+    {
+        public int population { get; set; }
+        public string region { get; set; }
+        public List<string> capital { get; set; }
+        public name name { get; set; }
+        public string subregion { get; set; }
+        public List<string> tld { get; set; }
+        //  public List<string[]> currencies { get; set; }
+        public List<string> borders { get; set; }
+        public flags flags { get; set; }
+        public Dictionary<string, string> languages { get; set; }
+    }
+
+    public class name
+    {
+        public string common { get; set; }
+
+        public string official { get; set; }
+
+    }
+
+    public class flags
+    {
+        public string png { get; set; }
+        public string svg { get; set; }
+    }
+    public class Filtro {
+    public string filtro { get; set; }
+    }
+
+    //public class Countries
+    //{
+    //    public List<CountryData> countries { get; set; } //not used
+    //}
+
+    //public class CountryData
+    //{
+    //    public string flag { get; set; }
+    //    public string name { get; set; }
+    //    public string capital { get; set; }
+    //    public List<currencies> Currencies { get; set; } //list of official currencies
+    //    public int population { get; set; }
+    //    public string alpha3Code { get; set; }
+    //    public List<languages> Languages { get; set; } //list of official languages
+    //}
+
+    //public class currencies
+    //{
+    //    public string name { get; set; }
+    //}
+
+    //public class languages
+    //{
+    //    public string name { get; set; }
+    //}
+
+}
